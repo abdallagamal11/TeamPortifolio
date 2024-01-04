9fb1a3b169aaa8ca1ed551eedd2f50bab3b0e6ba
abdal <abdallagamal117@gmail.com>
2024-01-04 14:08:01 +0200

    added sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,155 +1,85 @@
-let getTop = () => {
-	return (
-		(window.scrollY || document.documentElement.scrollTop) -
-		(document.documentElement.clientTop || 0)
-	);
+// var swiper = new Swiper('.swiper-container', {
+//     direction: 'vertical',
+//     sliderPerView: 1,
+//     spaceBetween: 0,
+//     mousewheel: true,
+//     pagination: {
+//         el:'.swiper-pagination',
+//         type: 'progressbar',
+//     },
+// 	autoHeight: true
+// });
+
+let isScrolledVisible = (elm) => {
+	let windowTop = window.scrollY;
+	let windowBottom = windowTop + window.innerHeight;
+
+	let elmTop = elm.offsetTop;
+	let elmBottom = elmTop + elm.offsetHeight;
+	let elmHalfY = elmTop + elm.offsetHeight / 2;
+
+	return elmTop <= windowBottom && elmHalfY <= windowBottom;
 };
 
-class Pager {
-	inPropation = false;
-	totalHeight;
-	pageHeight;
-	lastScrollTop;
-	numPages = 0;
-	pages = [];
-	page = 0;
+var counterFired = false;
+let adjustCounter = () => {
+	if (
+		!counterFired &&
+		isScrolledVisible(document.querySelector(".counter-section"))
+	) {
+		let valueDisplays = document.querySelectorAll(".num");
+		let interval = 100;
 
-	constructor() {
-		this.totalHeight = document.body.offsetHeight;
-		this.pageHeight = window.innerHeight;
-
-		this.lastScrollTop = getTop();
-		this.numPages = this.totalHeight / this.pageHeight;
-
-		let offset = 0;
-		for (let i = 0; i < this.numPages; i++) {
-			if (this.lastScrollTop >= offset) this.page = i;
-			this.pages.push(offset);
-			offset += this.pageHeight;
-		}
-		console.log(this.pages);
-		console.log(this.lastScrollTop);
-	}
-
-	scrollDown() {
-		if (!this.pages[this.page + 1]) return;
-		let nextPageOffset = this.pages[this.page + 1];
-		this.page++;
-
-		window.scrollTo({
-			top: nextPageOffset,
-			left: 0,
-			behavior: "smooth",
-		});
-
-		this.inPropation = true;
-		this.lastScrollTop = nextPageOffset;
-	}
-
-	scrollUp() {
-		if (this.page == 0) return;
-
-		let prevPageOffset = this.pages[this.page - 1];
-		this.page--;
-
-		window.scrollTo({
-			top: prevPageOffset,
-			left: 0,
-			behavior: "smooth",
-		});
-		this.inPropation = true;
-
-		this.lastScrollTop = prevPageOffset;
-	}
-
-	handleWheelMove(e) {
-	//	e.preventDefault();
-		e.stopPropagation();
-
-		if (!this.inPropation) {
-			if (e.deltaY > 0) {
-				this.scrollDown();
-			} else {
-				this.scrollUp();
-			}
-		}
-		setTimeout(() => (this.inPropation = false), 1000);
-	}
-
-	/*
-	 * Mobile Part of Pager
-	 */
-	startX = 0;
-	startY = 0;
-
-	handleTouchStart(event) {
-		this.startX = event.touches[0].clientX;
-		this.startY = event.touches[0].clientY;
-	}
-
-	handleTouchMove(event) {
-		
-		const currentX = event.touches[0].clientX;
-		const currentY = event.touches[0].clientY;
-
-		const deltaX = currentX - this.startX;
-		const deltaY = currentY - this.startY;
-
-		// Adjust sensitivity by changing the threshold
-		const threshold = 50;
-
-		if (Math.abs(deltaX) > threshold) {
-			// Horizontal swipe detected
-			if (deltaX > 0) {
-				console.log("Swipe right");
-			} else {
-				console.log("Swipe left");
-			}
-		} else if (Math.abs(deltaY) > threshold) {
-			// Vertical swipe detected
-			console.log('handleTouchMove');
-			if (!this.inPropation) {
-				if (deltaY > 0) {
-					console.log("Swipe down");
-					this.scrollUp();
-				} else {
-					console.log("Swipe up");
-					this.scrollDown();
+		valueDisplays.forEach((valueDisplay) => {
+			let startValue = 0;
+			let endValue = parseInt(valueDisplay.getAttribute("data-value"));
+			let duration = Math.floor(interval / endValue);
+			let counter = setInterval(() => {
+				startValue += 1;
+				valueDisplay.textContent = startValue;
+				if (startValue == endValue) {
+					clearInterval(counter);
 				}
-			}
-			setTimeout(() => (this.inPropation = false), 1000);
-		}
-
-		// Reset start coordinates for the next swipe
-		this.startX = currentX;
-		this.startY = currentY;
+			}, duration);
+		});
+		counterFired = true;
 	}
+};
+
+var navbar = document.querySelector(".navbar");
+
+let scrollAction = () => {
+	adjustCounter();
+	console.log(window.scrollY, navbar.offsetTop);
+	if (window.scrollY > navbar.offsetTop) {
+		navbar.classList.add("scroll");
+	} else {
+		navbar.classList.remove("scroll");
+	}
+};
+
+window.addEventListener("load", adjustCounter);
+document.addEventListener("scroll", scrollAction);
+
+// Animated Typing
+const div = document.querySelector("#companyname");
+const text = div.innerText;
+div.innerText = "";
+
+function TextTypingEffect(element, text, i = 0) {
+	if (i === 0) {
+		element.textContent = "";
+	}
+
+	element.textContent += text[i];
+
+	if (i === text.length - 1) {
+		return;
+	}
+
+	setTimeout(() => {
+		TextTypingEffect(element, text, i + 1);
+	}, 50);
 }
 
-window.addEventListener("load", () =>
-{
-	var pager = new Pager();
-
-	document.addEventListener("wheel", (e) => pager.handleWheelMove(e));
-
-	// if mobile
-	document.addEventListener("touchstart", (e) => pager.handleTouchStart(e));
-	document.addEventListener("touchmove", (e) => pager.handleTouchMove(e));
-
-	document.addEventListener('scrollend', (e) => 
-	{
-		setTimeout(() =>
-		{
-			let pageOffset = -1;
-			let positionTop = getTop();
-			pager.pages.forEach((p) => {
-				if(positionTop == p) pageOffset == p;
-			});
-			console.log(pageOffset);
-			if (pageOffset == -1)
-			{
-				let closest = pager.pages.reduce((prev, curr) => (Math.abs(curr - positionTop) < Math.abs(prev - positionTop) ? curr : prev));
-				window.scrollTo(0, closest);
-			}}, 100);
-	});
-});
+TextTypingEffect(div, text);
